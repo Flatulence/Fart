4ae6a5dee35088ca6a8861a4ab935ad4ace5560f
Dylan <dxa4481.rit.edu>
2015-05-05 22:28:31 -0400

    starting proxy server

--- fart/__init__.py
+++ fart/__init__.py
@@ -1,10 +1,16 @@
 """Main entry point
 """
 from pyramid.config import Configurator
-
+from libmproxy import controller, proxy
+from libmproxy.proxy.server import ProxyServer
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include("cornice")
     config.scan("fart.views")
     return config.make_wsgi_app()
+
+config = proxy.ProxyConfig(port=8080)
+server = ProxyServer(config)
+m = StickyMaster(server)
+m.run()
