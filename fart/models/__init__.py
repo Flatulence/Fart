26d6c67b212385702bfe407069fe046929f03533
Mike Nolan <me@michael-nolan.com>
2015-05-13 19:47:09 -0400

    added flag and pep8

--- fart/models/__init__.py
+++ fart/models/__init__.py
@@ -4,9 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 def initialize_sql():
     Session = sessionmaker()
-    engine = create_engine('sqlite:///:memory:', echo=True)
+    engine = create_engine('sqlite:///mypoop.db', echo=True,
+                           connect_args={'check_same_thread': False})
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
     Session.configure(bind=engine)
