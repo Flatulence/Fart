12d28ad18c2772137981d7fcc28388d9f58abe7f
Dylan <dxa4481@rit.edu>
2015-05-13 20:59:38 -0400

    mock history

--- fart/views.py
+++ fart/views.py
@@ -4,17 +4,17 @@ from cornice import Service
 
 
 hello = Service(name='hello', path='/', description="Simplest app")
-get_history = Service(name='get_history', path='/api/get_history')
+history = Service(name='get_history', path='/api/get_history')
 
 @hello.get()
 def get_info(request):
     """Returns Hello in JSON."""
     return {'Hello': 'World'}
 
-@get_history.get()
+@history.get()
 def get_history(request):
-    params = request.GET()
-    if page in params:
+    params = request.GET
+    if "page" in params:
         try:
             page = int(params["page"])
             return ["httpTransaction1", "httpTransaction2"]
